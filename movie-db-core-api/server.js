89bd238ca64f81f31d6f9038a12a71e1bbfd58e8
Sébastien VILLEBRUN <104035897+SVILLEBRUN@users.noreply.github.com>
2024-10-21 17:42:08 +0200

    Better code lisibility server.js

--- movie-db-core-api/server.js
+++ movie-db-core-api/server.js
@@ -1,10 +1,26 @@
-require('dotenv').config()
-
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors');
+require('dotenv').config()
 
+const app = express()
+
+// Database connection
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+
+// Routes
+const moviesRouter = require('./routes/movies')
+const configRouter = require('./routes/config')
+const proxyRouter = require('./routes/proxy')
+app.use('/api/movies', moviesRouter)
+app.use('/api/config', configRouter)
+app.use('/proxy', proxyRouter)
+
+// Enable CORS
 const corsOptions = {
 	origin: process.env.VUE_APP_URL, // Remplacez par l'URL de votre application front-end
 	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -12,16 +28,6 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
-
 app.use(express.json())
-
-const moviesRouter = require('./routes/movies')
-const configRouter = require('./routes/config')
-app.use('/api/movies', moviesRouter)
-app.use('/api/config', configRouter)
 
 app.listen(3000, '0.0.0.0', () => console.log('Server Started'))
