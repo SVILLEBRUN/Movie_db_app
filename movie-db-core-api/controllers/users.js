040d89404a5a32249ec5127d720fbfdc0f41b5a0
Sébastien VILLEBRUN <104035897+SVILLEBRUN@users.noreply.github.com>
2025-03-20 18:36:06 +0100

    Refacto for better code structure and architecture

--- movie-db-core-api/controllers/users.js
+++ movie-db-core-api/controllers/users.js
@@ -1,6 +1,7 @@
 const Users = require('../models/users')
 const bcrypt = require('bcrypt')
 const authUtils = require('../utils/auth')
+const jwt = require('jsonwebtoken');
 
 
 exports.createUser = async (req, res) => {
@@ -28,4 +29,47 @@ exports.createUser = async (req, res) => {
             return res.status(500).json({ message: err.message });
         }
     }
+}
+
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await Users.findOne({ email });
+        if(user && await bcrypt.compare(password, user.password)) {
+            authUtils.setCookie(res, user)
+            
+            return res.status(200).json({ 
+                message: 'User logged in', 
+                user: {
+                    _id: user._id,
+                    email: user.email,
+                    first_name: user.first_name,
+                    last_name: user.last_name
+                } 
+            });
+        } else {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+}
+
+
+exports.checkAuth = async (req, res) => {
+    const token = req.cookies.movie_db_nuxt_token;
+    if(!token) return res.status(401).json({ message: 'Unauthorized' });
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        return res.status(200).json({ message: 'User is authenticated', user: decoded });
+    } catch (err) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+}
+
+
+exports.logout = async (req, res) => {
+    authUtils.removeCookie(res)
+    return res.status(200).json({ message: 'User logged out' });
 }
--- movie-db-core-api/routes/auth.js
+++ movie-db-core-api/routes/auth.js
@@ -1,57 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const Users = require('../models/users');
 const userController = require('../controllers/users');
-const bcrypt = require('bcrypt');
-const authUtils = require('../utils/auth')
-const jwt = require('jsonwebtoken');
-
 
 
 
 router.post('/register', userController.createUser)
 
-// TODO: Put this in a controller
-router.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await Users.findOne({ email });
-        if(user && await bcrypt.compare(password, user.password)) {
-            authUtils.setCookie(res, user)
-            
-            return res.status(200).json({ 
-                message: 'User logged in', 
-                user: {
-                    _id: user._id,
-                    email: user.email,
-                    first_name: user.first_name,
-                    last_name: user.last_name
-                } 
-            });
-        } else {
-            return res.status(401).json({ message: 'Unauthorized' });
-        }
-    } catch (err) {
-        return res.status(500).json({ message: err.message })
-    }
-})
+router.post('/login', userController.login)
 
-// TODO: Put this in a controller
-router.get('/check-auth', (req, res) => {
-    const token = req.cookies.movie_db_nuxt_token;
-    if(!token) return res.status(401).json({ message: 'Unauthorized' });
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        return res.status(200).json({ message: 'User is authenticated', user: decoded });
-    } catch (err) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
-})
+router.get('/check-auth', userController.checkAuth)
 
-// TODO: Put this in a controller
-router.post('/logout', (req, res) => {
-    authUtils.removeCookie(res)
-    return res.status(200).json({ message: 'User logged out' });
-})
+router.post('/logout', userController.logout)
 
 module.exports = router
