84c6ae3179fbcca074fc0719a4a9b8183e1f35ba
Sébastien VILLEBRUN <104035897+SVILLEBRUN@users.noreply.github.com>
2025-06-03 03:15:36 +0200

    change bcrypt to bcryptjs to avoid compilations problems

--- movie-db-core-api/controllers/users.js
+++ movie-db-core-api/controllers/users.js
@@ -1,5 +1,5 @@
 const Users = require('../models/users')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const authUtils = require('../utils/auth')
 const jwt = require('jsonwebtoken');
 const { OAuth2Client } = require('google-auth-library');
